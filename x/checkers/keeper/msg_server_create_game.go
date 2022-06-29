41751de43a9f69586a4974a8c008b014782a8a8a
KiuHJ <jordkam13@gmail.com>
2022-06-29 07:22:00 +0000

    track all newly created files

---
+++ x/checkers/keeper/msg_server_create_game.go
@@ -0,0 +1,60 @@
+package keeper
+
+import (
+	"context"
+	rules "github.com/alice/checkers/x/checkers/rules"
+	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"strconv"
+)
+
+func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+		panic("NextGame not found")
+	}
+	newIndex := strconv.FormatUint(nextGame.IdValue, 10)
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Creator: msg.Creator,
+		Index:   newIndex,
+		Game:    newGame.String(),
+		Turn:    rules.PieceStrings[newGame.Turn],
+		Red:     msg.Red,
+		Black:   msg.Black,
+		MoveCount: 0,
+		BeforeId:  types.NoFifoIdKey,
+		AfterId:   types.NoFifoIdKey,
+		Deadline:  types.FormatDeadline(types.GetNextDeadline(ctx)),
+	   	Winner:    rules.PieceStrings[rules.NO_PLAYER],
+	}
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+	k.Keeper.SendToFifoTail(ctx, &storedGame, &nextGame)
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	nextGame.IdValue++
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	// what to emit
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
+			sdk.NewAttribute(sdk.AttributeKeyAction, types.StoredGameEventKey),
+			sdk.NewAttribute(types.StoredGameEventCreator, msg.Creator),
+			sdk.NewAttribute(types.StoredGameEventIndex, newIndex),
+			sdk.NewAttribute(types.StoredGameEventRed, msg.Red),
+			sdk.NewAttribute(types.StoredGameEventBlack, msg.Black),
+		),
+	)
+
+
+	return &types.MsgCreateGameResponse{
+		IdValue: newIndex,
+	}, nil
+}
---
+++ x/checkers/keeper/msg_server_reject_game.go
@@ -0,0 +1,62 @@
+package keeper
+
+import (
+	"context"
+        "strings"
+
+	rules "github.com/alice/checkers/x/checkers/rules"
+        "github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+
+func (k msgServer) RejectGame(goCtx context.Context,  msg *types.MsgRejectGame) (*types.MsgRejectGameResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+    // TODO: Handling the message
+        storedGame, found := k.Keeper.GetStoredGame(ctx, msg.IdValue)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "game not found %s", msg.IdValue)
+	}
+	// Is the game already won? Here, likely because it is forfeited.
+	if storedGame.Winner != rules.PieceStrings[rules.NO_PLAYER] {
+		return nil, types.ErrGameFinished
+	}
+
+	// Is it an expected player? And did the player already play?
+	if strings.Compare(storedGame.Red, msg.Creator) == 0 {
+		if 1 < storedGame.MoveCount {
+			return nil, types.ErrRedAlreadyPlayed
+		}
+	} else if strings.Compare(storedGame.Black, msg.Creator) == 0 {
+		if 0 < storedGame.MoveCount {
+			return nil, types.ErrBlackAlreadyPlayed
+		}
+	} else {
+		return nil, types.ErrCreatorNotPlayer
+	}
+
+	// Remove from the FIFO
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+		panic("NextGame not found")
+	}
+	k.Keeper.RemoveFromFifo(ctx, &storedGame, &nextGame)
+
+	// Remove the game completely as it is not interesting to keep it.
+	k.Keeper.RemoveStoredGame(ctx, msg.IdValue)
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	// What to emit
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
+			sdk.NewAttribute(sdk.AttributeKeyAction, types.RejectGameEventKey),
+			sdk.NewAttribute(types.RejectGameEventCreator, msg.Creator),
+			sdk.NewAttribute(types.RejectGameEventIdValue, msg.IdValue),
+		),
+	)
+
+	return &types.MsgRejectGameResponse{}, nil
+}
